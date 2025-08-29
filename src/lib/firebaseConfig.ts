1013dae51401fa2d0b140f96d750d9eafd1c428f
Mick Nassar <micx.nasty@gmail.com>
2025-08-29 07:40:09 +0000

    cleaning up

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -2,11 +2,11 @@
 // See https://firebase.google.com/docs/projects/learn-more#config-files-objects
 
 export const firebaseConfig = {
-  projectId: "willow-wind-warbler",
-  appId: "1:570302028471:web:91542c35b713a126a04b32",
-  storageBucket: "willow-wind-warbler.appspot.com",
-  apiKey: "AIzaSyDmHfoBHDkHfIgz5tBysAVtJyltKQqCXvw",
-  authDomain: "willow-wind-warbler.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "570302028471"
+  projectId: process.env.PROJECT_ID,
+  appId: process.env.APP_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  measurementId: process.env.MEASUREMENT_ID,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
 };
