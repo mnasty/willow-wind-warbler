9f5da3b289aff781db822bd8e0bb4efe82a12580
Mick Nassar <micx.nasty@gmail.com>
2025-08-29 07:52:41 +0000

    my application is not loading the firebase API key and associated proper

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -2,11 +2,11 @@
 // See https://firebase.google.com/docs/projects/learn-more#config-files-objects
 
 export const firebaseConfig = {
-  projectId: process.env.PROJECT_ID,
-  appId: process.env.APP_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  measurementId: process.env.MEASUREMENT_ID,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID
 };
