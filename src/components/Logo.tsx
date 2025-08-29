c4119a984e174d1320f9fe4d89f6dca90217bd4a
Mick Nassar <micx.nasty@gmail.com>
2025-08-29 03:57:13 +0000

    can you add more detail to the barn and silo logo so that it looks more

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,16 +11,23 @@ const Logo = (props: SVGProps<SVGSVGElement>) => (
     strokeLinejoin="round"
     {...props}
   >
-    {/* Barn */}
-    <path d="M4 21h10V9l-5-4-5 4v12z" fill="hsl(var(--secondary))" stroke="hsl(var(--foreground))"/>
-    <path d="M4 21h10" stroke="hsl(var(--foreground))" strokeLinecap="butt"/>
-    <path d="M7 15h4v6h-4v-6z" fill="hsl(var(--background))" stroke="hsl(var(--foreground))"/>
-    <path d="M7 18h4"/>
-    <path d="M9 15v-1"/>
-
     {/* Silo */}
-    <path d="M15 21V9c0-2.21 1.34-4 3-4h0c1.66 0 3 1.79 3 4v12" fill="hsl(var(--accent))" stroke="hsl(var(--foreground))"/>
-    <path d="M15 9c0-2.21 1.34-4 3-4h0c1.66 0 3 1.79 3 4" stroke="hsl(var(--foreground))"/>
+    <path d="M14 21V8.5c0-2.5 1.5-4.5 3.5-4.5s3.5 2 3.5 4.5V21" fill="hsl(var(--accent))" stroke="hsl(var(--foreground))" />
+    <path d="M21 9c-2.5 0-3.5-2-3.5-4.5" />
+    <path d="M14 9c2.5 0 3.5-2 3.5-4.5" />
+    <path d="M14 13h7" />
+    <path d="M14 17h7" />
+
+    {/* Barn */}
+    <path d="M3 21h10V9l-5-4-5 4v12z" fill="hsl(var(--secondary))" stroke="hsl(var(--foreground))"/>
+    <path d="M13 21H3" strokeLinecap="butt"/>
+    {/* Barn Door */}
+    <path d="M6 21V15h4v6" stroke="hsl(var(--foreground))" />
+    <path d="M6 15l4 6" />
+    <path d="M10 15l-4 6" />
+    
+    {/* Window */}
+    <path d="M8 12.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" fill="hsl(var(--foreground))" stroke="hsl(var(--foreground))" />
   </svg>
 );
 
