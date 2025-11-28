69e348c7449284d15a4d39b1f45f1aa419c93295
Mick Nassar <micx.nasty@gmail.com>
2025-11-28 06:30:04 +0000

    updated google description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 
 export const metadata: Metadata = {
   title: 'Willow Wind Warbler',
-  description: 'Hot off the digital press! The Willow Wind Warbler is your one-stop nest for all the family chirps, tweets, and tales. Get the scoop before it flies the coop!',
+  description: 'The Willow Wind Warbler is your one-stop nest for all the family chirps, tweets, and tales. Get the scoop before it flies the coop!',
 };
 
 export default function RootLayout({
